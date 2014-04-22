791442b85ae8c88c7e6aac4d058a74652e628b58
sergiogonza90 <sergiogonza90@23aacd88-60d1-5285-e027-765075025c5c>
2014-04-22 02:55:02 +0000

    Query 3

--- trunk/ base-datos-g3/Scripts/QUERIES.sql
+++ trunk/ base-datos-g3/Scripts/QUERIES.sql
@@ -2,7 +2,29 @@
 	
 -- Cantidad de leyes promulgadas en cada sesión en los últimos tres años
 	-- | sesión | cantidad de leyes promulgadas|
-	SELECT fecha_sancionada, COUNT(fecha_sancionada) FROM Ley WHERE (YEAR(DATE_SUB(CURRENT_DATE(), fecha_sancionada))) < 3 GROUP BY fecha_sancionada;
+
+	SELECT fecha_sancionada, COUNT(fecha_sancionada) 
+	    FROM Ley 
+	   WHERE (YEAR(DATE_SUB(CURRENT_DATE(), fecha_sancionada))) < 3 
+	GROUP BY fecha_sancionada;
 
 -- Diez legisladores con más incremento porcentual desde que se iniciaron en el cargo
 
+	SELECT dni_legislador, suma_total/suma_antes_cargo as Incremento
+	  FROM
+		( SELECT l.dni_legislador, SUM(b.valor) AS suma_total
+			FROM Bienes_de_legislador l, Bien_economico b
+		   WHERE l.id_bien_economico = b.id_bien_economico
+			 AND fecha_sucesion IS NULL	-- descarto los que ya no tiene mas
+		GROUP BY l.dni_legislador) s1
+		NATURAL JOIN
+		 (SELECT l.dni_legislador, SUM(b.valor) AS suma_antes_cargo
+			FROM Bienes_de_legislador l, Bien_economico b
+		   WHERE l.id_bien_economico = b.id_bien_economico
+			 AND fecha_sucesion IS NULL	-- descarto los que ya no tiene mas
+			 AND fecha_obtencion < (SELECT MAX(fecha_inicio)
+									 FROM Periodos_del_legislador
+									WHERE dni_legislador = l.dni_legislador)
+		GROUP BY l.dni_legislador) s2
+	ORDER BY suma_total/suma_antes_cargo
+	LIMIT 10;
