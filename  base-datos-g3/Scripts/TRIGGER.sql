9dd7e0d1cd8442e94dc47800211f8eec2ed0ffba
sergiogonza90 <sergiogonza90@23aacd88-60d1-5285-e027-765075025c5c>
2014-04-23 04:10:38 +0000

    agregado de estado_votaciones -> 'M'

---  base-datos-g3/Scripts/TRIGGER.sql
+++  base-datos-g3/Scripts/TRIGGER.sql
@@ -7,7 +7,7 @@ CREATE TRIGGER convertir_proyecto_en_ley AFTER UPDATE ON Proyecto_de_ley
 	BEGIN
 		DECLARE votos_positivos INT;
 		DECLARE votos_negativos INT;
-		IF OLD.estado_votaciones = 'A' and NEW.estado_votaciones = 'C'
+		IF OLD.estado_votaciones in ('M', 'A') and NEW.estado_votaciones = 'C'
 		THEN
 			SET votos_positivos = ( SELECT count(1) 
 									  FROM Votan v
