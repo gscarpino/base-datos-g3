7c6aa19050206b9531eed239abbdcbc3ecccd45a
gino.scarpino@gmail.com <gino.scarpino@gmail.com@23aacd88-60d1-5285-e027-765075025c5c>
2014-05-24 21:56:51 +0000

    nada importante

--- trunk/ base-datos-g3/ base-datos-g3/tp2/src/ejemplo.py
+++ trunk/ base-datos-g3/ base-datos-g3/tp2/src/ejemplo.py
@@ -4,9 +4,14 @@ import estimators
 # Creo una instancia de la clase que representa al metodo
 # 'Histograma Clasico'
 aEstimator = estimators.ClassicHistogram('db.sqlite3', 'table1', 'c1', parameter=20)
+bEstimator = estimators.DistributedSteps('db.sqlite3', 'table1', 'c1', parameter=100)
 
 # Pruebo distintas instancias de estimacion
 print "Classic Histogram"
 print "  Sel(=%d) : %3.2f" % (50, aEstimator.estimate_equal(50))
 print "  Sel(>%d) : %3.2f" % (70, aEstimator.estimate_greater(70))
+
+print "Pasos Distribuidos"
+print "  Sel(=%d) : %3.2f" % (50, bEstimator.estimate_equal(50))
+print "  Sel(>%d) : %3.2f" % (70, bEstimator.estimate_greater(70))
 
