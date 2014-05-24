ba6ccaf37fb4ac72bb8042e68f7cf8ec85e61022
gino.scarpino@gmail.com <gino.scarpino@gmail.com@23aacd88-60d1-5285-e027-765075025c5c>
2014-05-24 21:56:51 +0000

    nada importante

---  base-datos-g3/ base-datos-g3/tp2/src/estimators.py
+++  base-datos-g3/ base-datos-g3/tp2/src/estimators.py
@@ -71,4 +71,52 @@ class ClassicHistogram(Estimator):
 		for i in range(indice+1,len(self.buckets)):
 			acumulador = acumulador + self.buckets[i]
 		return acumulador
+
+
+class DistributedSteps(Estimator):
+	"""Pasos distribuidos"""
+	
+	def build_struct(self):
+		self.buckets = [0] * self.parameter
+		conexion = sqlite3.connect(self.db)
+		c = conexion.cursor()
+		
+		c.execute("Select min(" + self.column + ") From " + self.table + ";")
+		min = c.fetchone()
+		print "Minimo: " + str(min)
+		
+		c.execute("Select max(" + self.column + ") From " + self.table + ";")
+		max = c.fetchone()
+		print "Maximo: " + str(max)
+		
+		rango = max[0] - min[0]
+		
+		self.anchoBucket = rango / self.parameter
+		cantidad = 0
+		for fila in c.execute("Select " + self.column + " From " + self.table + ";"):
+			indice = fila[0] / self.anchoBucket 
+			if(indice == len(self.buckets)):
+				indice = indice - 1
+			self.buckets[indice] = self.buckets[indice] + 1
+			cantidad = cantidad + 1
+		
+		#print "Parametro: " + str(self.parameter) + " - Ancho: " + str(self.anchoBucket)
+		#print self.buckets
+		#print "Suma: " + str(sum(self.buckets))
+
+
+	def estimate_equal(self,value):
+		indice = value / self.anchoBucket
+		if(indice == len(self.buckets)):
+			indice = indice - 1
+		return self.buckets[indice]
+		
+	def estimate_greater(self,value):
+		indice = value / self.anchoBucket
+		if(indice == len(self.buckets)):
+			indice = indice - 1
+		acumulador = 0
+		for i in range(indice+1,len(self.buckets)):
+			acumulador = acumulador + self.buckets[i]
+		return acumulador
 	
