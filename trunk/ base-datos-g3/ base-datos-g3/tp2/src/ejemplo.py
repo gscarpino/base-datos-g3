90a7d32c1cfc194930cb1c2ab3b3d04865907203
gino.scarpino@gmail.com <gino.scarpino@gmail.com@23aacd88-60d1-5285-e027-765075025c5c>
2014-05-25 06:03:52 +0000

    Implementado la estructura del estimador de pasos distribuidos, falta el calculo de selectividad

--- trunk/ base-datos-g3/ base-datos-g3/tp2/src/ejemplo.py
+++ trunk/ base-datos-g3/ base-datos-g3/tp2/src/ejemplo.py
@@ -4,9 +4,9 @@ import estimators
 # Creo una instancia de la clase que representa al metodo
 exacto = estimators.Exacto('db.sqlite3', 'table1', 'c1')
 # 'Histograma Clasico'
-aEstimator = estimators.ClassicHistogram('db.sqlite3', 'table1', 'c1', parameter=200)
+aEstimator = estimators.ClassicHistogram('db.sqlite3', 'table1', 'c1', parameter=20)
 # 'Pasos Distribuidos'
-bEstimator = estimators.DistributedSteps('db.sqlite3', 'table1', 'c1', parameter=200)
+bEstimator = estimators.DistributedSteps('db.sqlite3', 'table1', 'c1', parameter=20)
 
 # Pruebo distintas instancias de estimacion
 print "Classic Histogram"
