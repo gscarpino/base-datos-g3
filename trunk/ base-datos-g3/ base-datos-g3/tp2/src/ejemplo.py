5273254477dec3c7f8de50c93480f5bd2b40eee1
gino.scarpino@gmail.com <gino.scarpino@gmail.com@23aacd88-60d1-5285-e027-765075025c5c>
2014-05-27 00:51:38 +0000

    pasos distribuidos

--- trunk/ base-datos-g3/ base-datos-g3/tp2/src/ejemplo.py
+++ trunk/ base-datos-g3/ base-datos-g3/tp2/src/ejemplo.py
@@ -4,20 +4,19 @@ import estimators
 # Creo una instancia de la clase que representa al metodo
 exacto = estimators.Exacto('db.sqlite3', 'table1', 'c1')
 # 'Histograma Clasico'
-aEstimator = estimators.ClassicHistogram('db.sqlite3', 'table1', 'c1', parameter=20)
+aEstimator = estimators.ClassicHistogram('db.sqlite3', 'table1', 'c1', parameter=100)
 # 'Pasos Distribuidos'
-bEstimator = estimators.DistributedSteps('db.sqlite3', 'table1', 'c1', parameter=20)
+bEstimator = estimators.DistributedSteps('db.sqlite3', 'table1', 'c1', parameter=100)
 
 # Pruebo distintas instancias de estimacion
 print "Classic Histogram"
-print "  Sel(=%d) : %3.2f" % (50, aEstimator.estimate_equal(50))
-print "  Sel(>%d) : %3.2f" % (70, aEstimator.estimate_greater(70))
+print "  Sel(=%d) : %3.5f" % (50, aEstimator.estimate_equal(50))
+print "  Sel(>%d) : %3.5f" % (70, aEstimator.estimate_greater(70))
 
 print "Pasos Distribuidos"
-print "  Sel(=%d) : %3.2f" % (50, bEstimator.estimate_equal(93))
-print "  Sel(>%d) : %3.2f" % (70, bEstimator.estimate_greater(70))
+print "  Sel(=%d) : %3.5f" % (50, bEstimator.estimate_equal(50))
+print "  Sel(>%d) : %3.5f" % (70, bEstimator.estimate_greater(70))
 
 print "Exacto"
-print "  Sel(=%d) : %3.2f" % (50, exacto.estimate_equal(50))
-print "  Sel(>%d) : %3.2f" % (70, exacto.estimate_greater(70))
-
+print "  Sel(=%d) : %3.5f" % (50, exacto.estimate_equal(50))
+print "  Sel(>%d) : %3.5f" % (70, exacto.estimate_greater(70))
