ce6daf8cc533ab04d320ce7566baec770e72f168
gino.scarpino@gmail.com <gino.scarpino@gmail.com@23aacd88-60d1-5285-e027-765075025c5c>
2014-05-27 00:51:38 +0000

    pasos distribuidos

---  base-datos-g3/ base-datos-g3/tp2/src/estimators.py
+++  base-datos-g3/ base-datos-g3/tp2/src/estimators.py
@@ -29,7 +29,11 @@ class Exacto(Estimator):
 	"""Metodo exacto"""
 
 	def build_struct(self):
-		pass
+		conexion = sqlite3.connect(self.db)
+		c = conexion.cursor()
+		c.execute("Select count(" + self.column + ") From " + self.table + ";")
+		self.total = c.fetchone()[0]
+		conexion.close()
 
 	def estimate_equal(self,value):
 		conexion = sqlite3.connect(self.db)
@@ -38,7 +42,7 @@ class Exacto(Estimator):
 		c.execute("Select count(" + self.column + ") From " + self.table + " Where " + self.column + " = " + str(value)  + ";")
 		resultado = c.fetchone()[0]
 		conexion.close()
-		return resultado
+		return 1.0 * resultado / self.total
 		
 	def estimate_greater(self,value):
 		conexion = sqlite3.connect(self.db)
@@ -47,7 +51,7 @@ class Exacto(Estimator):
 		c.execute("Select count(" + self.column + ") From " + self.table + " Where " + self.column + " > " + str(value)  + ";")
 		resultado = c.fetchone()[0]
 		conexion.close()
-		return resultado
+		return 1.0 * resultado  / self.total
 
 class ClassicHistogram(Estimator):
 	"""Histograma clasico"""
@@ -58,12 +62,15 @@ class ClassicHistogram(Estimator):
 		c = conexion.cursor()
 		
 		c.execute("Select min(" + self.column + ") From " + self.table + ";")
-		min = c.fetchone()
+		min = c.fetchone()[0]
 		
 		c.execute("Select max(" + self.column + ") From " + self.table + ";")
-		max = c.fetchone()
+		max = c.fetchone()[0]
 		
-		rango = max[0] - min[0]
+		c.execute("Select max(" + self.column + ") From " + self.table + ";")
+		self.total = c.fetchone()[0]
+		
+		rango = max - min
 		
 		self.anchoBucket = rango / self.parameter
 		c.execute("Select " + self.column + " From " + self.table + ";")
@@ -87,7 +94,7 @@ class ClassicHistogram(Estimator):
 		indice = value / self.anchoBucket
 		if(indice == len(self.buckets)):
 			indice = indice - 1
-		return self.buckets[indice]
+		return (1.0*self.buckets[indice] / self.total) / 100.0
 		
 	def estimate_greater(self,value):
 		indice = value / self.anchoBucket
@@ -96,7 +103,7 @@ class ClassicHistogram(Estimator):
 		acumulador = 0
 		for i in range(indice + 1,len(self.buckets)):
 			acumulador = acumulador + self.buckets[i]
-		return acumulador
+		return (1.0*acumulador / self.total) / 100.0
 
 
 class DistributedSteps(Estimator):
@@ -108,18 +115,18 @@ class DistributedSteps(Estimator):
 		c = conexion.cursor()
 		
 		c.execute("Select count(" + self.column + ") From " + self.table + ";")
-		total = c.fetchone()
+		self.total = c.fetchone()[0]
 		
 		c.execute("Select min(" + self.column + ") From " + self.table + ";")
-		self.minimo = c.fetchone()
+		self.minimo = c.fetchone()[0]
 		
 		c.execute("Select max(" + self.column + ") From " + self.table + ";")
-		self.maximo = c.fetchone()
+		self.maximo = c.fetchone()[0]
 		
-		self.anchoBucket = total[0] / self.parameter
+		self.anchoBucket = self.total / self.parameter
 		c.execute("Select " + self.column + " From " + self.table + " Order By " + self.column + " Asc;")
 		contador = 0
-    contador2 = 0
+		contador2 = 0
 		bucketActual = 0
 		temp = 0
 		#testing = [0] * self.parameter
@@ -131,24 +138,24 @@ class DistributedSteps(Estimator):
 					self.buckets[bucketActual] = [temp,contador]
 				break
 			else:
-        if(contador2 == bucketActual * ((self.total - 1) / self.parameter)):
-          self.buckets[bucketActual][0] = fila[0]
+				if(contador == bucketActual):
+					temp2 = fila[0]
 				if(contador < self.anchoBucket):
 					contador = contador + 1
-          contador2 = contador2 + 1
-          temp = fila[0]
+					temp = fila[0]
 				else:
-					self.buckets[bucketActual][1] = contador+1
+					self.buckets[bucketActual] = [temp2,contador+1]
 					contador = 0
-          contador2 = contador2 + 1
 					bucketActual = bucketActual + 1
+			contador2 = contador2 + 1
 		conexion.close()
-    #por las dudas, revisar si hace falta
-		self.buckets[0][0] = self.minimo[0]
-    self.buckets[len(self.buckets)-1][1] = self.maximo
+		#por las dudas, revisar si hace falta
+		#self.buckets[0][0] = self.minimo
+		#self.buckets[len(self.buckets)-1][1] = self.maximo
+		#print "Total: " + str(self.total)
 		
 		#print "Parametro: " + str(self.parameter) + " - Ancho: " + str(self.anchoBucket)
-		print self.buckets
+		#print self.buckets
 		#print testing
 		#print "Suma: " + str(sum(self.buckets))
 
@@ -156,32 +163,84 @@ class DistributedSteps(Estimator):
 	def estimate_equal(self,value):
 		s = 0
 		resultado = 0
-		if(self.maximo < value):
+		if(self.maximo < value or value < self.minimo):
 			resultado = 0
 		else:
 			while( self.buckets[s][0] < value ):
 				s = s + 1
 			if(s == 0):
 				if(value == self.buckets[0][0]):
-					resultado = self.buckets[0][1]
+					#repeticiones0 es cuantos steps del mismo valor incluyendo el inicial
+					repeticiones0 = 1
+					s = s + 1
+					while( self.buckets[s][0] == value ):
+						repeticiones0 = repeticiones0 + 1
+						s = s + 1
+					resultado = (repeticiones0 - 0.5) / self.total
 				else:
 					resultado = 0
 			else:
-				if(self.buckets[s][0] == value):
-					repeticiones = 1
-					s = s + 1
-					while( self.buckets[s][0] == value ):
-						repeticiones = repeticiones + 1
-						s = s + 1
-  					resultado = self.buckets[s-1][1]
+				if(s == (len(self.buckets)-1)):
+					resultado = (0.5 / self.parameter)
 				else:
-					#Esta entre buckets
-					#dudo que este bien pero es lo que dice el paper
-          #yo haria: resultado = self.buckets[s-1][1] / 2
-					resultado =  ((1/3) / self.parameter) * 100
-				
+					if(self.buckets[s][0] == value):
+						repeticiones = 1
+						s = s + 1
+						while( self.buckets[s][0] == value ):
+							repeticiones = repeticiones + 1
+							s = s + 1
+						if(s == (len(self.buckets)-1)):
+							#caso de repeticiones de steps incluyendo el ultimo
+							resultado = (((1.0 * repeticiones) - 0.5) / self.parameter)
+						else:
+							#repeticiones de steps pero no incluye al 1ero ni al ultimo
+							resultado = ((1.0 * repeticiones) / self.parameter)
+					else:
+						#Esta entre steps
+						resultado =  ((1.0/3.0) / self.parameter) 
 		return resultado
 		
 	def estimate_greater(self,value):
-		return 0
+		s = 0
+		resultado = 0
+		if(self.maximo <= value):
+			resultado = 0
+		else:
+			if(value < self.minimo):
+				resultado = 1
+			else:
+				while( self.buckets[s][0] < value ):
+					s = s + 1
+				if(s == 0):
+					if(value == self.buckets[0][0]):
+						#repeticiones0 es cuantos steps del mismo valor incluyendo el inicial
+						repeticiones0 = 1
+						s = s + 1
+						while( self.buckets[s][0] == value ):
+							repeticiones0 = repeticiones0 + 1
+							s = s + 1
+						resultado = 1 - (((1.0 * repeticiones0) - 0.5) / self.parameter)
+					else:
+						#esta demas
+						resultado = 0
+				else:
+					if(s == (len(self.buckets)-1)):
+						resultado = (0.5 / self.parameter)
+					else:
+						if(self.buckets[s][0] == value):
+							repeticiones = 1
+							s = s + 1
+							while( self.buckets[s][0] == value ):
+								repeticiones = repeticiones + 1
+								s = s + 1
+							if(s == (len(self.buckets)-1)):
+								#caso de repeticiones de steps incluyendo el ultimo
+								resultado = 0 
+							else:
+								#repeticiones de steps pero no incluye al 1ero ni al ultimo
+								resultado = 1 - ((s - 0.5) / self.parameter) - self.estimate_equal(value)
+						else:
+							#Esta entre steps
+							resultado =  1- ((1.0 * s + (1.0/3.0)) / self.parameter) -self.estimate_equal(value)
+		return resultado
 	
