ad420de11ffc0b7c6ac3604cb360a4fd2765da33
gino.scarpino@gmail.com <gino.scarpino@gmail.com@23aacd88-60d1-5285-e027-765075025c5c>
2014-06-14 19:28:54 +0000

    Corregidos varios errores de los estimadores. Anda mal el greater del estimador de pasos distribuidos

---  base-datos-g3/ base-datos-g3/tp2/src/ejemplo.py
+++  base-datos-g3/ base-datos-g3/tp2/src/ejemplo.py
@@ -2,26 +2,48 @@
 import estimators
 
 # Creo una instancia de la clase que representa al metodo
-exacto = estimators.Exacto('db.sqlite3', 'table1', 'c1')
+#exacto = estimators.Exacto('db.sqlite3', 'table1', 'c1')
 # 'Histograma Clasico'
-aEstimator = estimators.ClassicHistogram('db.sqlite3', 'table1', 'c1', parameter=3)
+#aEstimator = estimators.ClassicHistogram('db.sqlite3', 'table1', 'c1', parameter=20)
 # 'Pasos Distribuidos'
-bEstimator = estimators.DistributedSteps('db.sqlite3', 'table1', 'c1', parameter=3)
+#bEstimator = estimators.DistributedSteps('db.sqlite3', 'table1', 'c1', parameter=20)
 
-cEstimator = estimators.Entropia('db.sqlite3', 'table1', 'c1', parameter=3)
+#cEstimator = estimators.Entropia('db.sqlite3', 'table1', 'c1', parameter=3)
 # Pruebo distintas instancias de estimacion
-print "Classic Histogram"
-print "  Sel(=%d) : %3.5f" % (50, aEstimator.estimate_equal(50))
-print "  Sel(>%d) : %3.5f" % (70, aEstimator.estimate_greater(70))
+#~ print "Classic Histogram"
+#~ print "  Sel(=%d) : %3.5f" % (50, aEstimator.estimate_equal(666))
+#~ print "  Sel(>%d) : %3.5f" % (70, aEstimator.estimate_greater(70))
 
-print "Pasos Distribuidos"
-print "  Sel(=%d) : %3.5f" % (50, bEstimator.estimate_equal(50))
-print "  Sel(>%d) : %3.5f" % (70, bEstimator.estimate_greater(70))
+#~ print "Pasos Distribuidos"
+#~ print "  Sel(=%d) : %3.5f" % (50, bEstimator.estimate_equal(666))
+#~ print "  Sel(>%d) : %3.5f" % (70, bEstimator.estimate_greater(70))
 
-print "Exacto"
-print "  Sel(=%d) : %3.5f" % (50, exacto.estimate_equal(50))
-print "  Sel(>%d) : %3.5f" % (70, exacto.estimate_greater(70))
+#~ print "Implementacion Propia"
+#~ print "  Sel(=%d) : %3.15f" % (50, cEstimator.estimate_equal(666))
+#~ print "  Sel(>%d) : %3.15f" % (70, cEstimator.estimate_greater(70))
 
-print "Entropia"
-print "  Sel(=%d) : %3.5f" % (50, cEstimator.estimate_equal(50))
-#print "  Sel(>%d) : %3.5f" % (70, cEstimator.estimate_greater(70))
+#~ print "Exacto"
+#~ print "  Sel(=%d) : %3.5f" % (50, exacto.estimate_equal(666))
+#~ print "  Sel(>%d) : %3.5f" % (70, exacto.estimate_greater(70))
+
+
+print "#Buckets;Exacto;Clasico;Steps;"
+for numBuckets in range(100,1001,100):
+	exacto = estimators.Exacto('db.sqlite3', 'table1', 'c3')
+	aEstimator = estimators.ClassicHistogram('db.sqlite3', 'table1', 'c3', numBuckets)
+	bEstimator = estimators.DistributedSteps('db.sqlite3', 'table1', 'c3', numBuckets)
+	
+	valores = [1611,314,2865,-291,-1070]
+	for valor in valores:
+		print str(numBuckets) + ";" + str(exacto.estimate_equal(valor)) + ";" + str(aEstimator.estimate_equal(valor)) + ";" + str(bEstimator.estimate_equal(valor)) + ";"
+		
+
+print "#Buckets;Exacto;Clasico;Steps;"
+for numBuckets in range(100,1001,100):
+	exacto = estimators.Exacto('db.sqlite3', 'table1', 'c3')
+	aEstimator = estimators.ClassicHistogram('db.sqlite3', 'table1', 'c3', numBuckets)
+	bEstimator = estimators.DistributedSteps('db.sqlite3', 'table1', 'c3', numBuckets)
+	
+	valores = [1611,314,2865,-291,-1070]
+	for valor in valores:
+		print str(numBuckets) + ";" + str(exacto.estimate_greater(valor)) + ";" + str(aEstimator.estimate_greater(valor)) + ";" + str(bEstimator.estimate_greater(valor)) + ";"
