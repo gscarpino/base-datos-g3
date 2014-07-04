4a16dfecb6d5c7bd3d3014af89f75575125b3251
gino.scarpino@gmail.com <gino.scarpino@gmail.com@23aacd88-60d1-5285-e027-765075025c5c>
2014-07-04 20:13:18 +0000

    correcciones

---  base-datos-g3/tp1/entregable/QUERIES.sql
+++  base-datos-g3/tp1/entregable/QUERIES.sql
@@ -1,18 +1,20 @@
 
 
 -- Diputados que solo votaron positivo o ausente en los proyectos de ley de la comisión que integran
-select distinct leg.nombre, leg.dni, est.titulo_proyecto_ley, pro.titulo_proyecto_ley, est.nombre_comision, part.nombre_comision
-from estudia est, proyecto_de_ley pro, participa_en_comision part, legislador leg
-where est.titulo_proyecto_ley = pro.titulo_proyecto_ley and est.nombre_comision = part.nombre_comision
+select distinct leg.nombre, leg.dni
+from estudia est, proyecto_de_ley pro, participa_en_comision part, legislador leg, ley l
+where (est.titulo_proyecto_ley = pro.titulo_proyecto_ley or est.titulo_proyecto_ley = l.titulo_proyecto_ley) and est.nombre_comision = part.nombre_comision
 	and part.fecha_inicio_participacion <= pro.fecha and part.fecha_fin_participacion >= pro.fecha
-	and part.dni_legislador = leg.dni and and pro.estado_votaciones = "C" and not exists
+	and part.dni_legislador = leg.dni and pro.estado_votaciones = 'C' and not exists
 		(select vot.dni from votan vot where pro.titulo_proyecto_ley = vot.titulo_proyecto_ley and vot.dni = leg.dni
-			and vot.id_voto in (10,11,20,21));
-															 
+			and vot.id_voto in (10,11,20,21))
+group by leg.dni
+order by leg.nombre;
+			
 -- Cantidad de leyes promulgadas en cada sesión en los últimos tres años
 	-- | sesión | cantidad de leyes promulgadas|
 
-select ses.fecha_inicio_sesion, ses.fecha_fin_sesion, count(*)
+select ses.fecha_inicio_sesion, ses.fecha_fin_sesion, count(*) as cantidad
 from sesion ses, ley
 where ses.fecha_inicio_sesion <= ley.fecha_sancionada and ley.fecha_sancionada <= ses.fecha_fin_sesion 
 	and ((DATEDIFF(CURRENT_DATE(), ses.fecha_fin_sesion)) / 365) < 3
@@ -20,21 +22,21 @@ group by ses.fecha_inicio_sesion;
 
 -- Diez legisladores con más incremento porcentual desde que se iniciaron en el cargo
 
-	SELECT dni_legislador, suma_total/suma_antes_cargo as Incremento
-	  FROM
-		( SELECT l.dni_legislador, SUM(b.valor) AS suma_total
-			FROM Bienes_de_legislador l, Bien_economico b
-		   WHERE l.id_bien_economico = b.id_bien_economico
-			 AND fecha_sucesion IS NULL	-- descarto los que ya no tiene mas
-		GROUP BY l.dni_legislador) s1
-		NATURAL JOIN
-		 (SELECT l.dni_legislador, SUM(b.valor) AS suma_antes_cargo
-			FROM Bienes_de_legislador l, Bien_economico b
-		   WHERE l.id_bien_economico = b.id_bien_economico
-			 AND fecha_sucesion IS NULL	-- descarto los que ya no tiene mas
-			 AND fecha_obtencion < (SELECT MAX(fecha_inicio)
-									 FROM Periodos_del_legislador
-									WHERE dni_legislador = l.dni_legislador)
-		GROUP BY l.dni_legislador) s2
-	ORDER BY suma_total/suma_antes_cargo
-	LIMIT 10;
+select s1.dni, s1.sumaNuevo/s2.sumaTotal * 100 as porcentaje, s1.sumaNuevo, s2.sumaTotal
+from 
+	(select leg.dni, sum(bien.valor) as sumaNuevo
+	from bien_economico bien, bienes_del_legislador bLeg, legislador leg
+	where bien.id_bien_economico = bLeg.id_bien_economico and bLeg.dni_legislador = leg.dni
+		and bLeg.fecha_obtencion >= (select max(per.fecha_inicio) from periodos_del_legislador per where leg.dni = per.dni_legislador)
+		and bLeg.fecha_sucesion is NULL
+	group by leg.dni) s1 
+natural join
+	(select leg.dni, sum(bien.valor) as sumaTotal
+	from bien_economico bien, bienes_del_legislador bLeg, legislador leg
+	where bien.id_bien_economico = bLeg.id_bien_economico and bLeg.dni_legislador = leg.dni
+		and bLeg.fecha_obtencion < (select max(per.fecha_inicio) from periodos_del_legislador per where leg.dni = per.dni_legislador)
+		and (bLeg.fecha_sucesion is NULL or (bLeg.fecha_sucesion is not NULL and bLeg.fecha_sucesion > (select max(per.fecha_inicio) from periodos_del_legislador per where leg.dni = per.dni_legislador)))
+	group by leg.dni) s2
+group by s1.dni
+order by porcentaje desc
+limit 10;
