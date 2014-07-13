d236c51c8b054a54f90c55510800e3c96750b3c7
sergiogonza90 <sergiogonza90@23aacd88-60d1-5285-e027-765075025c5c>
2014-07-13 18:24:49 +0000

    Graficos de comparativos de comportamiento en distintas columnas, + chamuyo respectivo

--- trunk/ base-datos-g3/tp2/src/ejemplo.py
+++ trunk/ base-datos-g3/tp2/src/ejemplo.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import estimators
+import sqlite3
 #~ atributo = 'c3'
 # atributo = 'c1'
 # # Creo una instancia de la clase que representa al metodo
@@ -45,20 +46,40 @@ import estimators
 	#~ cEstimator = estimators.EstimatorGrupo('db.sqlite3', 'table1', atributo, numBuckets)
 	#~ for valor in valores:
 		#~ print str(numBuckets) + ";" + str(exacto.estimate_equal(valor)) + ";" + str(aEstimator.estimate_equal(valor)) + ";" + str(bEstimator.estimate_equal(valor)) + ";"+ str(cEstimator.estimate_equal(valor)) + ";"
+
+saltoBuckets = 10
+cantValores = 20
+#atributos = ['c0', 'c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8', 'c9']
+atributos = ['c8', 'c9']
+histogram = estimators.ClassicHistogram('db.sqlite3', 'table1', 'c8', 50)
+def mediaError(exacto, estimator, atributo):
+	conexion = sqlite3.connect('db.sqlite3')
+	c = conexion.cursor()
+	c.execute("Select min(" + atributo + ") From table1;")
+	minimo = c.fetchone()[0]
+	c.execute("Select max(" + atributo + ") From table1;")
+	maximo = c.fetchone()[0]
+	saltoValor = (maximo - minimo) / cantValores
+	acumuladorError = 0
+	for valor in range(minimo, maximo, saltoValor):
+		acumuladorError += abs( estimator.estimate_equal(valor) - exacto.estimate_equal(valor) )
+	return (1.0 * acumuladorError) / cantValores
 		
-salto = 10
-#~ valores = [-671,-402,323,699,940]
-#~ atributo = 'c2'
-valores = [32,150,461,704,927]
-atributo = 'c0'
-print "#Buckets;Exacto;Clasico;Steps;Propia;"
-for numBuckets in range(10,201,salto):
-	exacto = estimators.Exacto('db.sqlite3', 'table1', atributo)
-	aEstimator = estimators.ClassicHistogram('db.sqlite3', 'table1', atributo, numBuckets)
-	bEstimator = estimators.DistributedSteps('db.sqlite3', 'table1', atributo, numBuckets)
-	cEstimator = estimators.EstimatorGrupo('db.sqlite3', 'table1', atributo, numBuckets)
-	for valor in valores:
-		print str(numBuckets) + ";" + str(exacto.estimate_greater(valor)) + ";" + str(aEstimator.estimate_greater(valor)) + ";" + str(bEstimator.estimate_greater(valor)) + ";"+ str(cEstimator.estimate_greater(valor)) + ";"
+for atributo in atributos:
+	print "Atributo: ", atributo
+	print "#Buckets\tPromErrorClasico\tPromErrorSteps\tPromErrorGrupo"
+	for numBuckets in range(10, 120, saltoBuckets):
+		exacto = estimators.Exacto('db.sqlite3', 'table1', atributo)
+		histogram = estimators.ClassicHistogram('db.sqlite3', 'table1', atributo, numBuckets)
+		steps = estimators.DistributedSteps('db.sqlite3', 'table1', atributo, numBuckets)
+		grupo = estimators.EstimatorGrupo('db.sqlite3', 'table1', atributo, numBuckets)
+		mediaErrorHistogram = mediaError(exacto, histogram, atributo)
+		mediaErrorSteps = mediaError(exacto, steps, atributo)
+		mediaErrorGrupo = mediaError(exacto, grupo, atributo)
+		print str(numBuckets) + "\t" + '%f'%mediaErrorHistogram + "\t" + '%f'%mediaErrorSteps + "\t" + '%f'%mediaErrorGrupo
+	print ""
+
+
 
 #~ ''' Misma columna, mismo parametro, variacion de valor '''
 #~ numBuckets = 20
